512579a1e136e17e23ea923fab184fc5c86a37c1
Kyle McCloskey <mccloskeykyle@gmail.com>
2016-02-02 16:21:52 -0500

    animation on home screen

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,10 @@
-
 $(document).ready(function(){	
 	$(".delete-wizard").on("ajax:complete", function(e){
 		$(e.currentTarget).parent().hide()
+	});
+
+	$('#id_link').click( function () {
+		$(".sign_up_box").fadeOut(200)
+		$(".sign_in_box").delay(300).fadeIn(800)
 	});
 })
